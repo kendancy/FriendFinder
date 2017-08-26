fa0256dbee7cca4044c23c65f8d9970fff7af6d8
kendancy <kendancy@hotmail.com>
2017-08-26 08:22:27 -0700

    update friend selecting

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,25 @@
 //friends.js
 
 var surveyData = [
+
+      {
+  "name":"Wendy",
+  "photo":"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=imgres&cd=&cad=rja&uact=8&ved=0ahUKEwiz66_gxPLVAhVW4WMKHdnxD_8QjRwIBw&url=https%3A%2F%2Fgithub.com%2Fluomichelle%2FFriendFinder&psig=AFQjCNHOqAoAY77McVOuamMIV4dya3F4Vw&ust=1503755724250196",
+  "totalDiff":0,
+  "scores":[
+      5,
+      1,
+      2,
+      4,
+      1,
+      1,
+      3,
+      5,
+      4,
+      1
+    ]
+    },
+
     {
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
@@ -17,6 +36,27 @@ var surveyData = [
       4,
       1
     ]
-    }];
+    },
 
+    {
+  "name":"Mary",
+  "photo":"https://www.google.com/imgres?imgurl=https%3A%2F%2Favatars3.githubusercontent.com%2Fu%2F16859557%3Fv%3D4%26s%3D400&imgrefurl=https%3A%2F%2Fgithub.com%2Flvanok%2FFriendFinder&docid=DbIl3U4n6SqOYM&tbnid=Mf2g0rCl9xvXkM%3A&vet=10ahUKEwj-2LOwxPLVAhXk54MKHaDAC5EQMwgrKAQwBA..i&w=400&h=400&bih=781&biw=1418&q=%22photo%22%3A%22https%3A%2F%2Fmedia.licdn.com%2Fmpr%2Fmpr%2Fshrinknp_400_400%2Fp%2F6%2F005%2F064%2F1bd%2F3435aa3.jpg&ved=0ahUKEwj-2LOwxPLVAhXk54MKHaDAC5EQMwgrKAQwBA&iact=mrc&uact=8",
+  "totalDiff":0,
+  "scores":[
+      2,
+      5,
+      1,
+      1,
+      3,
+      2,
+      4,
+      5,
+      4,
+      2
+    ]
+    }
+
+];
+
+    
 module.exports = surveyData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,18 +5,41 @@ module.exports = function(app) {
 
     //get friends survey data
     app.get("/api/friends", function(req, res) {
-        // var totalDiff = 0;
-        // for (var i=0; i<surveyData.length; i++) {
-        //     surveyData.totalDiff += surveyData.scores[i];
-        // }
         res.json(surveyData);
-        res.json(true);
-        
     });
 
     //post friends survey data
     app.post("/api/friends", function(req, res) {
-        surveyData.push(req.body);
+
+        var closest = {
+            name: "",
+            photo: "",
+            total: 1000
+        }
+
+        var userData = req.body;
+        var userScores = userData.scores;
+        var totalDiff = 0;
+
+        for (var i=0; i < surveyData.length; i++) {
+    
+            var totalDiff = 0;
+ 
+            for (var j=0; j < surveyData[i].scores[j]; j++) {
+ 
+                totalDiff += Math.abs(parseInt(userScores[j]) - parseInt(surveyData[i].scores[j]));
+ 
+                if (totalDiff <= closest.total) {
+                    closest.name = surveyData[i].name;
+                    closest.photo = surveyData[i].photo;
+                    closest.total = surveyData[i].totalDiff;
+                }
+            }
+        }
+
+        surveyData.push(userData);
+        res.json(closest);
+ 
     });
 
 };
